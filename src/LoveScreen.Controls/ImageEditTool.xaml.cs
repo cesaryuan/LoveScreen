31f75204b7f23c1e3d4f0d71391b9baa3ba904c6
Cesaryuan <1052481067@qq.com>
2021-07-27 16:23:50 +0800

    Revert "待签出"
    This reverts commit 1f3b4b6ba146951516f8db24b8907205587e77ad.

--- src/LoveScreen.Controls/ImageEditTool.xaml.cs
+++ src/LoveScreen.Controls/ImageEditTool.xaml.cs
@@ -22,16 +22,6 @@ namespace LoveScreen.Controls
     /// </summary>
     public partial class ImageEditTool : UserControl
     {
-        public Rect HightLightRect
-        {
-            get { return (Rect)GetValue(HightLightRectProperty); }
-            set { SetValue(HightLightRectProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for HightLightRect.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty HightLightRectProperty =
-            DependencyProperty.Register("HightLightRect", typeof(Rect), typeof(ImageEditTool));
-
         public static readonly RoutedEvent SelectedColorChagnedEvent = ColorPicker.SelectedColorChagnedEvent.AddOwner(typeof(ImageEditTool));
 
         public static readonly RoutedEvent SelectedSizeChagnedEvent = SizePicker.SelectedSizeChagnedEvent.AddOwner(typeof(ImageEditTool));
@@ -180,13 +170,12 @@ namespace LoveScreen.Controls
             set { SetValue(SelectedSizeProperty, value); }
         }
 
-        public ExtendedInkCanvas InkCanvas = null;
         public ImageEditTool()
         {
             InitializeComponent();
-            this.InkCanvas = inkCanvas;
+
         }
-        
+
         private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
         {
             Rectangle rectangle = (Rectangle)sender;
